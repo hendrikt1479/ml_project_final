4fc5dfae8efc27eab0d1375ecd835fa7118fb6c7
hendrikt1479 <hendrik.tanaka@gmail.com>
2022-11-29 21:22:55 +0700

    trial api/streamlit

---
+++ src/api.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+import pandas as pd
+import numpy as np
+import yaml
+
+import data_pipeline as data_pipeline
+import preprocessing as preprocessing
+
+
+config = utils.load_config()
+model_data = utils.pickle_load(config['production_model_path'])
+
+class api_data(BaseModel):
+    Temperature : float
+    Humidity : float
+    Pressure : float
+    PM1 : float
+    TVOC : int
+    eCO2 : int
+    H2 : int
+    Ethanol : int
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return 'Hello, FastAPI up!'
+
+@app.post('/predict/')
+def predict(data: api_data):    
+    # Convert data api to dataframe
+    data = pd.DataFrame(data).set_index(0).T.reset_index(drop = True)  # type: ignore
+    data.columns = config['predictors']
+
+    # Convert dtype
+    data = pd.concat(
+        [
+            data[config['predictors'][:4]].astype(np.float64),  # type: ignore
+            data[config['predictors'][4:]].astype(np.int64)  # type: ignore
+        ],
+        axis = 1
+    )
+
+    # Check range data
+    try:
+        data_pipeline.check_data(data, config, True)  # type: ignore
+    except AssertionError as ae:
+        return {'res': [], 'error_msg': str(ae)}
+
+    # Predict data
+    y_pred = model_data.predict(data)
+
+    if y_pred[0] == 0:
+        y_pred = 'Tidak ada api.'
+    else:
+        y_pred = 'Ada api.'
+    return {'res' : y_pred, 'error_msg': ''}
+
+if __name__ == '__main__':
+    uvicorn.run('api:app', host = '0.0.0.0', port = 8080)
---
+++ src/streamlit.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import requests
+import yaml
+import joblib
+from PIL import Image
+
+# Load and set images in the first place
+header_images = Image.open('assets/banner.png')
+st.image(header_images)
+
+# Add some information about the service
+st.title('Smoke Prediction')
+st.subheader('Just enter variabel below then click Predict button :sunglasses:')
+
+# Create form of input
+with st.form(key = 'air_data_form'):
+    # Create box for number input
+    temperature = st.number_input(
+        label = '1.\tEnter Temperature[C] Value:',
+        min_value = -40,
+        max_value = 125,
+        help = 'Value range from -40 to 125'
+    )
+
+    humidity = st.number_input(
+        label = '2.\tEnter Humidity[%] Value:',
+        min_value = 0,
+        max_value = 100,
+        help = 'Value range from 0 to 100'
+    )
+    
+    pressure = st.number_input(
+        label = '3.\tEnter Pressure[hPa] Value:',
+        min_value = 300,
+        max_value = 1250,
+        help = 'Value range from 300 to 1250'
+    )
+
+    pm1 = st.number_input(
+        label = '4.\tEnter PM1.0 Value:',
+        min_value = 0,
+        max_value = 65535,
+        help = 'Value range from 0 to 65535'
+    )
+
+    tvoc = st.number_input(
+        label = '5.\tEnter TVOC[ppb] Value:',
+        min_value = 0,
+        max_value = 60000,
+        help = 'Value range from 0 to 60000'
+    )
+
+    eco2 = st.number_input(
+        label = '6.\tEnter eCO2[ppm] Value:',
+        min_value = 400,
+        max_value = 60000,
+        help = 'Value range from 400 to 60000'
+    )
+
+    h2 = st.number_input(
+        label = '7.\tEnter Raw H2 Value:',
+        min_value = 0,
+        max_value = 60000,
+        help = 'Value range from 0 to 60000'
+    )
+
+    ethanol = st.number_input(
+        label = '8.\tEnter Raw Ethanol Value:',
+        min_value = 0,
+        max_value = 60000,
+        help = 'Value range from 0 to 60000'
+    )
+    
+    # Create button to submit the form
+    submitted = st.form_submit_button('Predict')
+
+    # Condition when form submitted
+    if submitted:
+        # Create dict of all data in the form
+        raw_data = {
+            'Temperature': temperature,
+            'Humidity': humidity,
+            'Pressure': pressure,
+            'PM1': pm1,
+            'TVOC': tvoc,
+            'eCO2': eco2,
+            'H2': h2,
+            'Ethanol': ethanol
+        }
+
+        # Create loading animation while predicting
+        with st.spinner('Sending data to prediction server ...'):
+            res = requests.post('http://api_backend:8080/predict', json = raw_data).json()
+            
+        # Parse the prediction result
+        if res['error_msg'] != '':
+            st.error('Error Occurs While Predicting: {}'.format(res['error_msg']))
+        else:
+            if res['res'] != 'Tidak ada api.':
+                st.warning('Ada api.')
+            else:
+                st.success('Tidak ada api.')
